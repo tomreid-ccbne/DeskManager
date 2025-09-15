c1653b78999b094953a43c301ce5b5910e14fcb8
tomreid-ccbne <tom.reid@compareclub.com.au>
2025-09-15 11:56:28 +1000

    Create DeskLayout.vue

---
+++ src/components/DeskLayout.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useLayoutStore } from '../stores/layout';
+import Desk from './Desk.vue';
+
+const layoutStore = useLayoutStore();
+</script>
+
+<template>
+  <main 
+    class="relative bg-gray-200 dark:bg-gray-700 bg-cover bg-center rounded-lg shadow-lg transition-all" 
+    :style="{ 
+      width: `${layoutStore.settings.layoutWidth}%`, 
+      aspectRatio: layoutStore.settings.aspectRatio, 
+      backgroundImage: layoutStore.settings.imageVisible ? layoutStore.settings.backgroundImage : 'none' 
+    }"
+  >
+    <div 
+      class="relative w-full h-full" 
+      :style="{ 
+        transform: `scale(${layoutStore.settings.zoom / 100})`, 
+        transformOrigin: 'top left' 
+      }"
+    >
+      <Desk 
+        v-for="desk in layoutStore.desks" 
+        :key="desk.id" 
+        :desk="desk" 
+      />
+    </div>
+  </main>
+</template>
+
